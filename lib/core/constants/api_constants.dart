4d52a41ac77951199be260246bcc71d74402e747
HozaifaJawhar <hozaifajawhar@gmail.com>
2025-12-20 18:22:45 +0200

    delete test file

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,10 +1,10 @@
 class ApiConstants {
   // when you run the back-end localy
   // if you use real device uncomment the line 4 and put your ip on laptop using ifconfig/ipconfig + run the server using this command: php artisan serve --host 0.0.0.0 --port 8000 +  make sure that you phone and laptop are on the same wifi or network source.
-  // static const String baseUrl = "http://192.168.1.42:8000/api";
+  static const String baseUrl = "http://192.168.1.42:8000/api";
 
   // if you use emulator to run the app comment the line 4 and uncomment line 7
-  static const String baseUrl = "http://10.0.2.2:8000/api";
+  // static const String baseUrl = "http://10.0.2.2:8000/api";
 
 // Auth
   static const String loginUrl = "/login";
